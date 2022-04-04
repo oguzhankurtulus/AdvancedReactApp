b3d71e4c8a4499751707bde557c6d2a3d4419f56
Oguzhan Kurtulus <oguzhan.kurtulus@yandex.com>
2022-04-05 00:38:01 +0300

    Add styled components. Enable stylings for SSR.

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,18 @@
-import Document, { Html, NextScript, Main } from 'next/document';
+import Document, { Html, NextScript, Main, Head } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+    static getInitialProps({ renderPage }) {
+        const sheet = new ServerStyleSheet();
+        const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
+        const styleTags = sheet.getStyleElement();
+        return { ...page, styleTags };
+    }
+
     render() {
         return (
             <Html lang="en-CA">
-                {/* <Head></Head> */}
+                <Head />
                 <body>
                     <Main />
                     <NextScript />
