dd30676faf23f4049aaea538df752de1fe0d7046
Oguzhan Kurtulus <oguzhan.kurtulus@yandex.com>
2022-04-08 00:35:51 +0300

    Add Apollo Client config.

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,33 @@
+import { ApolloProvider } from '@apollo/client';
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import Page from '../components/Page';
+import withData from '../lib/withData';
+
+// Import Assets
 import '../components/styles/nprogress.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
     return (
-        <Page>
-            <Component {...pageProps} />
-        </Page>
+        <ApolloProvider client={apollo}>
+            <Page>
+                <Component {...pageProps} />
+            </Page>
+        </ApolloProvider>
     );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx);
+    }
+    pageProps.query = ctx.query;
+    return { pageProps };
+};
+
+export default withData(MyApp);
