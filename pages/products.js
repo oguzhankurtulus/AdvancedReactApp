8694a46d52c9794a636ba2236a07d41b7028a2b5
Oguzhan Kurtulus <oguzhan.kurtulus@yandex.com>
2022-04-08 17:29:00 +0300

    Add graph query to fetch data. Arrange Product page.

--- pages/products.js
+++ pages/products.js
@@ -1,7 +1,9 @@
+import Products from '../components/Products';
+
 export default function ProductsPage() {
-  return (
-    <div>
-      <p>Products Page</p>
-    </div>
-  );
+    return (
+        <div>
+            <Products />
+        </div>
+    );
 }
