1277ca3fd4a13d5df58504befffb84ac2681902b
Oguzhan Kurtulus <oguzhan.kurtulus@yandex.com>
2022-05-18 12:02:50 +0300

    Add removeFromCart component.

---
+++ components/RemoveFromCart.js
@@ -0,0 +1,34 @@
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import { useMutation } from '@apollo/client';
+
+const BigButton = styled.button`
+    font-size: 3rem;
+    background: none;
+    border: 0;
+    &:hover {
+        color: var(--red);
+        cursor: pointer;
+    }
+`;
+
+const REMOVE_FROM_CART_MUTATION = gql`
+    mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
+        deleteCartItem(id: $id) {
+            id
+        }
+    }
+`;
+
+function RemoveFromCart({ id }) {
+    const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
+        variables: { id },
+    });
+    return (
+        <BigButton type="button" title="Remove this item from cart" onClick={removeFromCart} disabled={loading}>
+            &times;
+        </BigButton>
+    );
+}
+
+export default RemoveFromCart;
