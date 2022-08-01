532ba7829a212bdc5249e7c121695c47c106155b
Oguzhan Kurtulus <oguzhan.kurtulus@yandex.com>
2022-08-01 13:50:10 +0300

    Add snapshot testing.

--- __test__/Product.test.js
+++ __test__/Product.test.js
@@ -21,4 +21,13 @@ describe('<Product />', () => {
         expect(link).toHaveAttribute('href', '/product/abc123');
         expect(link).toHaveTextContent(product.name);
     });
+
+    it('renders and matches the snapshot', () => {
+        const { container } = render(
+            <MockedProvider>
+                <Product product={product} />
+            </MockedProvider>
+        );
+        expect(container).toMatchSnapshot();
+    });
 });
