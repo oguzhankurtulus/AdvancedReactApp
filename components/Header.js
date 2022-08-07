a2f0cdb25ffe55ca88543142c2a7f4ade3b684c3
Oguzhan Kurtulus <oguzhan.kurtulus@yandex.com>
2022-08-07 19:12:54 +0300

    Add husky pre-commit hooks

--- components/Header.js
+++ components/Header.js
@@ -1,72 +1,72 @@
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import styled from 'styled-components';
-import Cart from './Cart';
-import Nav from './Nav';
-import Search from './Search';
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import styled from "styled-components";
+import Cart from "./Cart";
+import Nav from "./Nav";
+import Search from "./Search";
 
 const Logo = styled.h1`
-    font-size: 4rem;
-    margin-left: 2rem;
-    position: relative;
-    z-index: 2;
-    background: red;
-    transform: skew(-7deg);
-    a {
-        color: white;
-        text-decoration: none;
-        text-transform: uppercase;
-        padding: 0.5rem 1rem;
-    }
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
 `;
 
 const HeaderStyles = styled.header`
-    .bar {
-        border-bottom: 10px solid var(--black, black);
-        display: grid;
-        grid-template-columns: auto 1fr;
-        justify-content: space-between;
-        align-items: stretch;
-    }
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+  }
 
-    .sub-bar {
-        display: grid;
-        grid-template-columns: 1fr auto;
-        border-bottom: 1px solid var(--black, black);
-    }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
 `;
 
 function ClientOnly({ children, ...delegated }) {
-    const [hasMounted, setHasMounted] = useState(false);
+  const [hasMounted, setHasMounted] = useState(false);
 
-    useEffect(() => {
-        setHasMounted(true);
-    }, []);
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
 
-    if (!hasMounted) {
-        return null;
-    }
+  if (!hasMounted) {
+    return null;
+  }
 
-    return <div {...delegated}>{children}</div>;
+  return <div {...delegated}>{children}</div>;
 }
 
 function Header() {
-    return (
-        <HeaderStyles>
-            <div className='bar'>
-                <Logo className='logo'>
-                    <Link href='/'>Sick Fits</Link>
-                </Logo>
-                <Nav />
-            </div>
-            <div className='sub-bar'>
-                <ClientOnly>
-                    <Search />
-                </ClientOnly>
-            </div>
-            <Cart />
-        </HeaderStyles>
-    );
+  return (
+    <HeaderStyles>
+      <div className="bar">
+        <Logo className="logo">
+          <Link href="/">Sick Fits</Link>
+        </Logo>
+        <Nav />
+      </div>
+      <div className="sub-bar">
+        <ClientOnly>
+          <Search />
+        </ClientOnly>
+      </div>
+      <Cart />
+    </HeaderStyles>
+  );
 }
 
 export default Header;
