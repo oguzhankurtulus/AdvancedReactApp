52102475c9ff0b1eb04123e8e17780f9702da188
Oguzhan Kurtulus <oguzhan.kurtulus@yandex.com>
2022-08-08 14:20:45 +0300

    Fix eslint issues

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,9 +4,10 @@ module.exports = {
 
   // This will lint and format TypeScript and                                             //JavaScript files
   "**/*.(ts|tsx|js)": (filenames) => [
-    `yarn eslint --fix ${filenames.join(" ")}`,
     `yarn prettier --write ${filenames.join(" ")}`,
   ],
+
+  // `yarn eslint --fix ${filenames.join(' ')}`,
 
   // this will Format MarkDown and JSON
   "**/*.(md|json)": (filenames) =>
